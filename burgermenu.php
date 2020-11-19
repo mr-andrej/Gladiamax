62e9a3f0dc079b1e55b1fae65a6422a110e5a3d0
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2020-11-19 17:26:08 +0100

    Cut out and included the header.php and the burgermenu.php

---
+++ burgermenu.php
@@ -0,0 +1,69 @@
+<!-- Humberger Menu Begin -->
+<div class="humberger-menu-overlay"></div>
+<div class="humberger-menu-wrapper">
+    <div class="hw-logo">
+        <a href="#"><img src="img/f-logo.png" alt=""></a>
+    </div>
+    <div class="hw-menu mobile-menu">
+        <ul>
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="#">Pages <i class="fa fa-angle-down"></i></a>
+                <ul class="dropdown">
+                    <li><a href="./categories-list.html">Categories</a></li>
+                    <li><a href="./categories-grid.html">Categories grid</a></li>
+                    <li><a href="./typography.html">Typography</a></li>
+                    <li><a href="./details-post-default.html">Post default</a></li>
+                    <li><a href="./details-post-gallery.html">Post gallery</a></li>
+                    <li><a href="./details-post-review.html">Post review</a></li>
+                    <li><a href="./contact.html">Contact</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Reviews <i class="fa fa-angle-down"></i></a>
+                <ul class="dropdown">
+                    <li><a href="#">Playstation</a></li>
+                    <li><a href="#">Windows</a></li>
+                    <li><a href="#">Mobile</a></li>
+                    <li><a href="#">Xbox</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Windows <i class="fa fa-angle-down"></i></a>
+                <ul class="dropdown">
+                    <li><a href="#">Playstation</a></li>
+                    <li><a href="#">Windows</a></li>
+                    <li><a href="#">Mobile</a></li>
+                    <li><a href="#">Xbox</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Videos <i class="fa fa-angle-down"></i></a>
+                <ul class="dropdown">
+                    <li><a href="#">Playstation</a></li>
+                    <li><a href="#">Windows</a></li>
+                    <li><a href="#">Mobile</a></li>
+                    <li><a href="#">Xbox</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div id="mobile-menu-wrap"></div>
+    <div class="hw-copyright">
+        Copyright © 2019 Colorlib Inc. All rights reserved
+    </div>
+    <div class="hw-social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-youtube-play"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+    </div>
+    <div class="hw-insta-media">
+        <div class="section-title">
+            <h5>Instagram</h5>
+        </div>
+        <div class="insta-pic">
+            <img src="img/instagram/ip-1.jpg" alt="">
+            <img src="img/instagram/ip-2.jpg" alt="">
+            <img src="img/instagram/ip-3.jpg" alt="">
+            <img src="img/instagram/ip-4.jpg" alt="">
+        </div>
+    </div>
+</div>
+<!-- Humberger Menu End -->
